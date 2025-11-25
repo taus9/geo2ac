4252c90efda34021c651748af8f2ee2647f56a95
Christopher Martinez <crmartinez239@protonmail.com>
2025-11-25 18:14:48 -0500

    Add load message

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ with open(filename, 'r') as csvfile:
   csvreader = csv.reader(csvfile)
   for row in csvreader:
     rows.append(row)
-    
+  
+  print(filename + " successfully loaded")
   print("Total number of area codes: %d" % csvreader.line_num)
