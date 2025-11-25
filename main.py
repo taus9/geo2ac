ee8b79853d79fb0404194286d11c46fa77624397
Christopher Martinez <crmartinez239@protonmail.com>
2025-11-25 18:52:47 -0500

    Load API key from env

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 #https://github.com/ravisorg/Area-Code-Geolocation-Database/blob/master/us-area-code-geo.csv
 
+from dotenv import load_dotenv
+import os
+import sys
 import csv
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+if api_key == "None":
+  print("No API key found")
+  sys.exit(1)
 
 filename = "us-area-code-geo.csv"
 rows = []
+
+print(api_key)
 
 with open(filename, 'r') as csvfile:
   csvreader = csv.reader(csvfile)
   for row in csvreader:
     rows.append(row)
-  
-  print(filename + " successfully loaded")
-  print("Total number of area codes: %d" % csvreader.line_num)
+
+print(filename + " successfully loaded")
+print("Total number of area codes: %d" % csvreader.line_num)
